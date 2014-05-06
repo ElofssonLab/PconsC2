f1e7613da8586edba41bfbe0f68200e3a03b1cb6
Mirco Michel <mirco.michel@scilifelab.se>
2014-05-06 15:23:21 +0200

    Fix newline bug in a3mToXX.py scripts

--- scripts/a3mToJones.py
+++ scripts/a3mToJones.py
@@ -7,7 +7,9 @@ infile = open(infilef)
 
 for l in infile:
     if '>' in l:
+        sys.stdout.write('\n')
         continue
+    l = l.strip()
     upperseq = ''.join([c for c in l if not c.islower()])
     upperseq = upperseq.replace('X', '-')
     sys.stdout.write(upperseq)
