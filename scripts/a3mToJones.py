c897b544eb5342f265c78faad2a96a2f733e4196
Oxana <merenlin@gmail.com>
2014-05-12 15:05:25 +0200

    Fixed length mismatch in a3mtoJones

--- scripts/a3mToJones.py
+++ scripts/a3mToJones.py
@@ -6,13 +6,20 @@ infilef = sys.argv[1]
 infile = open(infilef)
 
 count = 0
+line = ""
+
 for l in infile:
     if '>' in l:
-        if count != 0:
-            sys.stdout.write('\n')
+        if (count != 0):
+           sys.stdout.write(line)
+           sys.stdout.write('\n')
+           line = ""
+
         count += 1
         continue
+
     l = l.strip()
+    
     upperseq = ''.join([c for c in l if not c.islower()])
     upperseq = upperseq.replace('X', '-')
-    sys.stdout.write(upperseq)
+    line = line + upperseq
